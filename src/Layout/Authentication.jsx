70bb5176bd3b8f846867fed14c7b1657cc7a77be
ashrafulhossen <b.asrafulhossencht@gmail.com>
2023-05-21 16:09:57 +0600

    Automatic navigate to private route when user logged in feature is done

--- src/Layout/Authentication.jsx
+++ src/Layout/Authentication.jsx
@@ -1,10 +1,18 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useContext } from "react";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { AuthContext } from "../AuthProvider/AuthProvider";
 import Footer from "../Page/Shared/Footer/Footer";
 import Header from "../Page/Shared/Header/Header";
 
 const Authentication = () => {
+	const { user } = useContext(AuthContext);
+	const location = useLocation();
+	const previousLocation = location?.state?.from?.pathname;
+
+	if (user && previousLocation) {
+		return <Navigate to={previousLocation} replace={true} />;
+	}
 	return (
 		<div>
 			<Header />
